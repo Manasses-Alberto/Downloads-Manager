84b32d93d7c05c586ca6e0fcaa5bf057327748d2
Manasses-Alberto <manassesndombele88@gmail.com>
2023-12-29 20:17:23 +0100

    Fiz a função de organizar os arquivos em subpastas com base na sua extensão

--- downloads_manager.py
+++ downloads_manager.py
@@ -1,39 +1,77 @@
-import os
-
-def create_sub_dirs(main_dir: str):
-    try:
-        os.makedirs(f'{main_dir}imagens')
-    except FileExistsError:
-        pass
-    try:
-        os.makedirs(f'{main_dir}vídeos')
-    except FileExistsError:
-        pass
-    try:
-        os.makedirs(f'{main_dir}áudios')
-    except FileExistsError:
-        pass
-    try:
-        os.makedirs(f'{main_dir}documentos')
-    except FileExistsError:
-        pass
+from shutil import move
+from os import makedirs, path, listdir
+from time import sleep
 
 while True:
     downloads_dir = str(input('Digite o caminho do diretório de downloads: '))
     if downloads_dir == '':
         print('\033[91mO caminho está vazio! Insira um caminho válido!\033[0m')
 
-    elif not os.path.isdir(downloads_dir):
-        print(f'\033[91mO caminho `{downloads_dir}` não é uma pasta\033[0m')
-    
-    elif downloads_dir[len(downloads_dir) - 1] != '/':
-        print(f'\033[91mO caminho `{downloads_dir} deve conter uma / no final.`')
-
-    elif not os.path.exists(downloads_dir):
+    elif not path.exists(downloads_dir):
         print(f'\033[91mO caminho `{downloads_dir}` não foi encontrado.\033[0m')
+
+    elif not path.isdir(downloads_dir):
+        print(f'\033[91mO caminho `{downloads_dir}` não é uma pasta\033[0m')
 
     else:
         print(f'\033[92m`{downloads_dir}` foi validado com sucesso!\033[0m')
         break
 
-create_sub_dirs(downloads_dir)
+file_types = {
+    '.jpg': 'Imagens',
+    '.jpeg': 'Imagens',
+    '.svg': 'Imagens',
+    '.png': 'Imagens',
+    '.gif': 'Imagens',
+    '.bmp': 'Imagens',
+    '.mp4': 'Videos',
+    '.avi': 'Videos',
+    '.mkv': 'Videos',
+    '.mov': 'Videos',
+    '.wmv': 'Videos',
+    '.flv': 'Videos',
+    '.mpg': 'Videos',
+    '.mpeg': 'Videos',
+    '.mp3': 'Audios',
+    '.wav': 'Audios',
+    '.flac': 'Audios',
+    '.aac': 'Audios',
+    '.ogg': 'Audios',
+    '.m4a': 'Audios',
+    '.wma': 'Audios',
+    '.aif': 'Audios',
+    '.aiff': 'Audios',
+    '.pdf': 'Documentos',
+    '.doc': 'Documentos',
+    '.docx': 'Documentos',
+    '.txt': 'Documentos',
+    '.pptx': 'Documentos',
+    '.xlsx': 'Documentos',
+    '.*': 'Outros',
+}
+
+for file_type in file_types.values():
+    dir = path.join(downloads_dir, file_type)
+    if not path.exists(dir):
+        makedirs(dir)
+
+files = listdir(downloads_dir)
+for file in files:
+    file_path = path.join(downloads_dir, file)
+    if path.isfile(file_path):
+        name, extension = path.splitext(file)
+        extension = extension.lower()
+
+        if extension in file_types:
+            destiny = path.join(downloads_dir, file_types[extension])
+        else:
+            destiny = path.join(downloads_dir, 'Outros', file)
+
+        move(path.join(downloads_dir, file), destiny)
+        sleep(2)
+        try:
+            print(f'\n\033[92mArquivo {file} movido para {file_types[extension]}.\033[0m\n')
+        except KeyError:
+            print(f'\n\033[38;5;208mArquivo {file} movido para o diretório Outros\033[0m\n')
+
+print('\033[94mTODOS OS ARQUIVOS FORAM MOVIDOS COM SUCESSO!\033[0m')
